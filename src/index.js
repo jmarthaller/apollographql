1b14a12dec2af0e8dac1c987c13327c4bb18e5d4
Jacob Marthaller <marthaller.jr@gmail.com>
2021-06-05 12:13:37 -0700

    Troubleshoot code runner

--- src/index.js
+++ src/index.js
@@ -13,13 +13,16 @@ import {
   });
 
 
-  client.query({
-      query: gql`
-        query GetRates {
-            rates(currency: "USD") {
-                currency
-            }
-        }
-      `
-  })
-  .then(result => console.log(result));
+ // const client = ...
+
+client
+.query({
+  query: gql`
+    query GetRates {
+      rates(currency: "USD") {
+        currency
+      }
+    }
+  `
+})
+.then(result => console.log(result));
