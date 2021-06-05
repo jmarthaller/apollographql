9a3345cb6978f633d4892a9eb0fd06f015f4f35f
Jacob Marthaller <marthaller.jr@gmail.com>
2021-06-05 13:08:29 -0700

    Reformat datatypes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,57 @@
+import React from 'react';
+import { render } from 'react-dom';
 import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    useQuery,
-    gql
-  } from "@apollo/client";
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
-  
-  const client = new ApolloClient({
-      uri: 'https://48p1r2roz4.sse.codesandbox.io',
-      cache: new InMemoryCache()
-  });
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
 
 
- // const client = ...
-
-client
-.query({
-  query: gql`
-    query GetRates {
-      rates(currency: "USD") {
-        currency
-      }
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
     }
-  `
-})
-.then(result => console.log(result));
+  }
+`;
+
+function ExchangeRates() {
+
+    const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+    // MAP THROUGH CURRENCY DATA
+    return data.rates.map(({ currency, rate }) => (
+      <div key={currency}>
+        <p>
+          {currency}: {rate}
+        </p>
+      </div>
+    ));
+  }
+  
+
+function App() {
+  return (
+    <div>
+      <h2>Currency Data via Apollo/GraphQL <span role="img" aria-label="rocket">🚀</span> -----&gt; <span role="img" aria-label="moon">🌕</span></h2>
+      <ExchangeRates />
+    </div>
+  );
+}
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
